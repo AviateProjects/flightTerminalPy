0de9d5f1afc1e22549a136049d2fbbaabb9033a2
AviateProjects <oliversbackup8088@gmail.com>
2022-05-10 15:47:06 -0500

    info section added

--- main.py
+++ main.py
@@ -15,7 +15,9 @@
         
 def selectStart():
     i=0
-    Items=("Play Now","Quit")
+    Items=("Play Now","Info","Quit")
+    print("")
+    print("Welcome to Flight Transport for Python terminal")
     while i<len(Items):
         print(str(i)+": "+Items[i])
         i=i+1
@@ -23,13 +25,27 @@ def selectStart():
 
     if s==0:
         selectStartPort()
+    elif s==1:
+        info()
     else:
         quit()
 
+def info():
+    print("")
+    print("Welcome to Flight Transport for Python terminal")
+    print("This game is in a Demo phase")
+    print("This game is in a sandbox mode")
+    print("more features are coming soon")
+    print("Update can be found at AviateProjects On github")
+    print("Made by Oliver W.")
+    print("Version 0.0b --FuntionMap Beta--")
+    selectStart()
+
 def selectStartPort():
-    i=0                              
+    i=0
+    Items=("Atlanta","London","Tokyo","Moscow","Quit") 
+    print("") 
     print("Starting Airports:")
-    Items=("Atlanta","London","Tokyo","Moscow","Quit")          
     while i<len(Items):            
         print(str(i)+": "+Items[i]) 
         i=i+1                       
@@ -45,5 +61,4 @@ def selectStartPort():
 def selectPortATL():
     i=input("hello world")
 
-print("Welcome to Flight Transport Game for Python terminal")
 selectStart()
