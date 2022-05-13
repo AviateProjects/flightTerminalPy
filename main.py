e880f68948af2f6ef4a39824b19752b24c49f147
AviateProjects <oliversbackup8088@gmail.com>
2022-05-12 22:23:03 -0500

    release 1.1

--- main.py
+++ main.py
@@ -1,23 +1,79 @@
-#Made By Oliver W. 5-10-22
+#Made By Oliver W. 5-12-22
 #More info can be found at
 #Github.com/AviateProjects
+# VERSION 1.1
+
+#selecting functions
+def select2(a,b,s):
+    #this function is for start()
+    if s==0:
+        a()
+    elif s==1:
+        b()
+    else:
+        quit()
+def select3(a,b,c,s):
+    if s==0:
+        a()
+    elif s==1:
+        b()
+    elif s==2:
+        c()
+    else:
+        quit()
+def select4(a,b,c,d,s):
+    if s==0:
+        a()
+    elif s==1:
+        b()
+    elif s==2:
+        c()
+    elif s==3:
+        d()
+    else:
+        quit()
+def select5(a,b,c,d,e,s):
+    if s==0:
+        a()
+    elif s==1:
+        b()
+    elif s==2:
+        c()
+    elif s==3:
+        d()
+    elif s==4:
+        e()
+    else:
+        quit()
+def select6(a,b,c,d,e,f,s):
+    if s==0:
+        a()
+    elif s==1:
+        b()
+    elif s==2:
+        c()
+    elif s==3:
+        d()
+    elif s==4:
+        e()
+    elif s==5:
+        f()
+    else:
+        quit()
+def lister(i,Items):
+    while i<len(Items):
+        print(str(i)+": "+Items[i])
+        i=i+1
+
 #Entrance Nodes
-def Start():
+def start():
     i=0
     Items=("Play Now","Info","Quit")
     print("")
     print("Welcome to Flight Transport for Python terminal")
-    while i<len(Items):
-        print(str(i)+": "+Items[i])
-        i=i+1
+    lister(i,Items)
     s=int(input("Select: "))
-
-    if s==0:
-        selectStartPort()
-    elif s==1:
-        info()
-    else:
-        quit()
+    select2(selectStartPort,info,s)
 def info():
     print("")
     print("Welcome to Flight Transport for Python terminal")
@@ -26,483 +82,201 @@ def info():
     print("more features are coming soon")
     print("Update can be found at AviateProjects On github")
     print("Made by Oliver W.")
-    print("Version 1.0b")
-    Start()
+    print("Version 1.1")
+    start()
 def selectStartPort():
     i=0
-    Items=("Atlanta","London","Tokyo","Sydney","Quit") 
-    print("") 
+    Items=("Atlanta","London","Tokyo","Sydney","Quit")
+    print("")
     print("Starting Airports:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)
     s=int(input("Select: "))
+    select4(portATL,portLHR,portHND,portSYD,s)
 
-    if s==0:                        
-        portATL()
-    elif s==1:                     
-        portLHR()
-    elif s==2:                     
-        portHND()
-    elif s==3:                     
-        portSYD()
-    else:                          
-       quit()
 #Airport nodes
 def portADD():
     i=0
-    Items=("Mumbai","Cairo","Cape Town","Lagos","Tunisia","Quit") 
-    print("") 
+    Items=("Mumbai","Cairo","Cape Town","Lagos","Tunisia","Quit")
+    print("")
     print("You are at Addis Adiba:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)
     s=int(input("Fly to: "))
-
-    if s==0:                        
-        portBOM()
-    elif s==1:                     
-        portCAI()
-    elif s==2:                     
-        portCPT()
-    elif s==3:                     
-        portLOS()
-    elif s==4:                     
-        portTUN()
-    else:                          
-        quit()
+    select5(portBOM,portCAI,portCPT,portLOS,portTUN,s)
 def portATL():
     i=0
-    Items=("New York","Los Angles","Mexico City","Quit") 
-    print("") 
+    Items=("New York","Los Angeles","Mexico City","Quit")
+    print("")
     print("You are at Atlanta:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)
     s=int(input("Fly to: "))
-    if s==0:                        
-        portJFK()
-    elif s==1:                     
-        portLAX()
-    elif s==2:                     
-        portMEX()
-    else:                          
-        quit()
+    select3(portJFK,portLAX,portMEX,s)
 def portBGW():
     i=0
-    Items=("Mumbai","Cairo","Delhi","Istanbul","Moscow","Quit") 
+    Items=("Mumbai","Cairo","Delhi","Istanbul","Moscow","Quit")
     print("") 
     print("You are at Bahgdad:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)                    
     s=int(input("Fly to: "))
-    if s==0:                        
-        portBOM()
-    elif s==1:                     
-        portCAI()
-    elif s==2:                     
-        portDEL()
-    elif s==3:                     
-        portIST()
-    elif s==4:                     
-        portSVO()
-    else:                          
-        quit()
+    select5(portBOM,portCAI,portDEL,portIST,portSVO,s)
 def portBOG():
     i=0
-    Items=("Rio De Jenero","New York","Lima","Mexico City","Quit") 
+    Items=("Rio De Jenero","New York","Lima","Mexico City","Quit")
     print("") 
     print("You are at Bogota:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)               
     s=int(input("Fly to: "))
-    if s==0:                        
-        portGIG()
-    elif s==1:                     
-        portJFK()
-    elif s==2:                     
-        portLIM()
-    elif s==3:                     
-        portMEX()
-    else:                          
-        quit()
+    select4(portGIG,portJFK,portLIM,portMEX,s)
 def portBOM():
     i=0
-    Items=("Addis Adaba","Baghdad","Cape Town","Delhi","Hong Kong","Sydney","Quit") 
+    Items=("Addis Adaba","Baghdad","Cape Town","Delhi","Hong Kong","Sydney","Quit")
     print("") 
     print("You are at Mumbai:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)                
     s=int(input("Fly to: "))
-    if s==0:                        
-        portADD()
-    elif s==1:                     
-        portBGW()
-    elif s==2:                     
-        portCPT()
-    elif s==3:                     
-        portDEL()
-    elif s==4:                     
-        portHKG()
-    elif s==5:                     
-        portSYD()
-    else:                          
-        quit()
+    select6(portADD,portBGW,portCPT,portDEL,portHKG,portSYD,s)
 def portCAI():
     i=0
-    Items=("Addis Adaba","Baghdad","Istanbul","Lagos","Tunisia","Quit") 
+    Items=("Addis Adaba","Baghdad","Istanbul","Lagos","Tunisia","Quit")
     print("") 
     print("You are at Cairo:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)                 
     s=int(input("Fly to: "))
-    if s==0:                        
-        portADD()
-    elif s==1:                     
-        portBGW()
-    elif s==2:                     
-        portIST()
-    elif s==3:                     
-        portLOS()
-    elif s==4:                     
-        portTUN()
-    else:                          
-        quit()
+    select5(portADD,portBGW,portIST,portLOS,portTUN,s)
 def portCPT():
     i=0
-    Items=("Addis Addiba","Mumbai","Rio De Jenero","Lagos","Sydney","Quit") 
+    Items=("Addis Addiba","Mumbai","Rio De Jenero","Lagos","Sydney","Quit")
     print("") 
     print("You are at CapeTown:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)                   
     s=int(input("Fly to: "))
-    if s==0:                        
-        portADD()
-    elif s==1:                     
-        portBOM()
-    elif s==2:                     
-        portGIG()
-    elif s==3:                     
-        portLOS()
-    elif s==4:                     
-        portSYD()
-    else:                          
-        quit()
+    select5(portADD,portBOM,portGIG,portLOS,portSYD,s)
 def portDEL():
     i=0
-    Items=("Baghdad","Mumbai","Hong Kong","Quit") 
+    Items=("Baghdad","Mumbai","Hong Kong","Quit")
     print("") 
     print("You are at New Delhi:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)     
     s=int(input("Fly to: "))
-
-    if s==0:                        
-        portBGW()
-    elif s==1:                     
-        portBOM()
-    elif s==2:                     
-        portHKG()
-    else:                          
-        quit()
+    select3(portBGW,portBOM,portHKG,s)
 def portGIG():
     i=0
-    Items=("Bogota","Cape Town","Lima","Lagos","Quit") 
+    Items=("Bogota","Cape Town","Lima","Lagos","Quit")
     print("") 
     print("You are at Rio De Jenero:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)               
     s=int(input("Fly to: "))
-    if s==0:                        
-        portGIG()
-    elif s==1:                     
-        portCPT()
-    elif s==2:                     
-        portLIM()
-    elif s==3:                     
-        portLOS()
-    else:                          
-        quit()
+    select4(portBOG,portCPT,portLIM,portLOS,s)
 def portHKG():
     i=0
-    Items=("Mumbai","Delhi","Tokyo","Honolulu","Bejing","Sydney","Quit") 
+    Items=("Mumbai","Delhi","Tokyo","Honolulu","Bejing","Sydney","Quit")
     print("") 
     print("You are at Hong Kong:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)                
     s=int(input("Fly to: "))
-    if s==0:                        
-        portBOM()
-    elif s==1:                     
-        portDEL()
-    elif s==2:                     
-        portHND()
-    elif s==3:                     
-        portHNL()
-    elif s==4:                     
-        portPEK()
-    elif s==5:                     
-        portSYD()
-    else:                          
-        quit()
+    select6(portBOM,portDEL,portHND,portHNL,portPEK,portSYD,s)
 def portHND():
     i=0
     Items=("Hong Kong","Honalulu","Bejing","Sydney","Quit") 
     print("") 
     print("You are at Tokyo:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)                 
     s=int(input("Fly to: "))
-    if s==0:                        
-        portHKG()
-    elif s==1:                     
-        portHNL()
-    elif s==2:                     
-        portPEK()
-    elif s==3:                     
-        portSYD()
-    else:                          
-        quit()
+    select4(portHKG,portHNL,portPEK,portSYD,s)
 def portHNL():
     i=0
     Items=("Hong Kong","Tokyo","Los Angeles","Lima","Mexico City","Sydney","Quit") 
     print("") 
     print("You are at Hong Kong:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)                     
     s=int(input("Fly to: "))
-    if s==0:                        
-        portHKG()
-    elif s==1:                     
-        portHND()
-    elif s==2:                     
-        portLAX()
-    elif s==3:                     
-        portLIM()
-    elif s==4:                     
-        portMEX()
-    elif s==5:                     
-        portSYD()
-    else:                          
-        quit()
+    select6(portHKG,portHND,portLAX,portLIM,portMEX,portSYD,s)
 def portIST():
     i=0
     Items=("Baghdad","Cairo","London","Moscow","Tunisia","Quit") 
     print("") 
     print("You are at Istanbul:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)                 
     s=int(input("Fly to: "))
-
-    if s==0:                        
-        portBGW()
-    elif s==1:                     
-        portCAI()
-    elif s==2:                     
-        portLHR()
-    elif s==3:                     
-        portSVO()
-    elif s==4:                     
-        portTUN()
-    else:                          
-        quit()
+    select5(portBGW,portCAI,portLHR,portSVO,portTUN,s)
 def portJFK():
     i=0
     Items=("Atlanta","Bogota","London","Lagos","Quit") 
     print("") 
     print("You are at New York:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)           
     s=int(input("Fly to: "))
-    if s==0:                        
-        portATL()
-    elif s==1:                     
-        portBOG()
-    elif s==2:                     
-        portLHR()
-    elif s==3:                     
-        portLOS()
-    else:                          
-        quit()
+    select4(portATL,portBOG,portLHR,portLOS,s)
 def portLAX():
     i=0
     Items=("Atlanta","Honolulu","Mexico City","Quit") 
     print("") 
     print("You are at Los Angeles:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)                
     s=int(input("Fly to: "))
-    if s==0:                        
-        portATL()
-    elif s==1:                     
-        portHNL()
-    elif s==2:                     
-        portMEX()
-    else:                          
-        quit()
+    select3(portATL,portHNL,portMEX,s)
 def portLHR():
     i=0
     Items=("Istanbul","New York","Moscow","Tunisia","Quit") 
     print("") 
     print("You are at London:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)                           
     s=int(input("Fly to: "))
-    if s==0:                        
-        portIST()
-    elif s==1:                     
-        portJFK()
-    elif s==2:                     
-        portSVO()
-    elif s==3:                     
-        portTUN()
-    else:                          
-        quit()
+    select4(portIST,portJFK,portSVO,portTUN,s)
 def portLIM():
     i=0
     Items=("Bogota","Rio De Jenero","Honalulu","Quit") 
     print("") 
     print("You are at Lima:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)                    
     s=int(input("Fly to: "))
-    if s==0:                        
-        portBOG()
-    elif s==1:                     
-        portGIG()
-    elif s==2:                     
-        portHNL()
-    else:                          
-        quit()
+    select3(portBOG,portGIG,portHNL,s)
 def portLOS():
     i=0
     Items=("Addis Addiba","Cairo","Cape Town","Rio De Jenero","New York","Quit") 
     print("") 
     print("You are at Lagos:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)                       
     s=int(input("Fly to: "))
-
-    if s==0:                        
-        portADD()
-    elif s==1:                     
-        portCAI()
-    elif s==2:                     
-        portCPT()
-    elif s==3:                     
-        portGIG()
-    elif s==4:                     
-        portJFK()
-    else:                          
-        quit()
+    select5(portADD,portCAI,portCPT,portGIG,portJFK,s)
 def portMEX():
     i=0
     Items=("Atlanta","Bogota","Honalulu","Los Angles","Quit") 
     print("") 
     print("You are at Mexico City:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)       
     s=int(input("Fly to: "))
-    if s==0:                        
-        portATL()
-    elif s==1:                     
-        portBOG()
-    elif s==2:                     
-        portHNL()
-    elif s==3:                     
-        portLAX()
-    else:                          
-        quit()
+    select4(portATL,portBOG,portHNL,portLAX,s)
 def portPEK():
     i=0
     Items=("Hong Kong","Honalulu","Moscow","Quit") 
     print("") 
     print("You are at Bejing:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)           
     s=int(input("Fly to: "))
-    if s==0:                        
-        portHKG()
-    elif s==1:                     
-        portHNL()
-    elif s==2:                     
-        portSVO()
-    else:                          
-        quit()   
+    select3(portHKG,portHNL,portSVO,s)
 def portSVO():
     i=0
     Items=("Bahgdad","Istanbul","London","Bejing","Quit") 
     print("") 
     print("You are at Moscow:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)           
     s=int(input("Fly to: "))
-    if s==0:                        
-        portBGW()
-    elif s==1:                     
-        portIST()
-    elif s==2:                     
-        portLHR()
-    elif s==3:                     
-        portPEK()
-    else:                          
-        quit()
+    select4(portBGW,portIST,portLHR,portPEK,s)
 def portSYD():
     i=0
     Items=("Mumbai","Cape Town","Hong Kong","Honnalulu","Quit") 
     print("") 
     print("You are at Sydney:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)            
     s=int(input("Fly to: "))
-    if s==0:                        
-        portBOM()
-    elif s==1:                     
-        portCPT()
-    elif s==2:                     
-        portHKG()
-    elif s==3:                     
-        portHNL()
-    else:                          
-        quit()
+    select4(portBOM,portCPT,portHKG,portHNL,s)
 def portTUN():
     i=0
     Items=("Addis Addiba","Cairo","Istanbul","London","Quit") 
     print("") 
     print("You are at Tunisia:")
-    while i<len(Items):            
-        print(str(i)+": "+Items[i]) 
-        i=i+1                       
+    lister(i,Items)        
     s=int(input("Fly to: "))
-    if s==0:                        
-        portADD()
-    elif s==1:                     
-        portCAI()
-    elif s==2:                     
-        portIST()
-    elif s==3:                     
-        portLHR()
-    else:                          
-        quit()
+    select4(portADD,portCAI,portIST,portLAX,s)
     
-Start()
+start()
